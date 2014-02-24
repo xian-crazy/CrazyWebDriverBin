bbf5d183c7686613c1a7db4bc203fdba213c9a4b
yh <Administrator@PC201310140854>
2014-02-24 10:45:13 +0800

    update

--- src/bin/MouseAction.java
+++ src/bin/MouseAction.java
@@ -5,18 +5,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-
 public class MouseAction {
+	private WebDriver driver = null;
 
-	public void mouseRightclickContext(WebDriver driver,WebElement element){
-		
-//		Actions actions = new Actions(driver);
-//		actions.contextClick(ffwb.findElement(By.xpath("//div/span[text()='收件箱']"))).perform();
-//		wait.waitFor(2000);
-//		ffwb.findElement(By.xpath("//div[contains(@id,'mail_menu')][last()]/descendant::span[text()='打开']")).click();
-//		wait.waitFor(2000);
-//		
-		
-		
+	public MouseAction(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	// 鼠标右键单击
+	public void rightClickTheMouse(WebElement element) {
+		Actions actions = new Actions(driver);
+		actions.contextClick(element).perform();
+	}
+
+	// 左键双击鼠标
+	public void doubleClickTheLeftMouseButton(WebElement element) {
+		Actions action = new Actions(driver);
+		action.doubleClick(element).perform();
+
+	}
+
+	// 鼠标拖动元素到指定元素上
+	public void dragElementToElementByMouse(WebElement source, WebElement target) {
+
+		Actions action = new Actions(driver);
+		action.dragAndDrop(source, target);
+
+	}
+	
+	// 将目标元素拖拽到指定的区域里
+	public void dragElementToArea(WebElement element, int xOffset,int yOffset) {
+		Actions action = new Actions(driver);
+		action.dragAndDropBy(element,xOffset,yOffset);
+
 	}
 }
--- src/bin/ScrollerAction.java
+++ src/bin/ScrollerAction.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ScrollerAction { 
-	WebDriver drr=null;
-	WebElement el=null;
-	public void dragAndDrop(WebDriver driver,WebElement e){
-		drr=driver;
-		el=e;
-		String id=el.getAttribute("id");
-		String js="var q=document.getElementById('"+id+"').scrollTop=10000";
+public class ScrollerAction {
+	private WebDriver drr = null;
+
+	public ScrollerAction(WebDriver driver) {
+		this.drr = driver;
+	}
+
+	public void dragAndDrop(WebElement e) {
+		String id = e.getAttribute("id");
+		String js = "var q=document.getElementById('" + id+ "').scrollTop=10000";
 		((JavascriptExecutor) drr).executeScript(js);
-		}
+	}
+	
+	public void dragAndDrop(WebElement e ,int px) {
+		String id = e.getAttribute("id");
+		String js = "var q=document.getElementById('" + id+ "').scrollTop="+px;
+		((JavascriptExecutor) drr).executeScript(js);
+	}
 }
